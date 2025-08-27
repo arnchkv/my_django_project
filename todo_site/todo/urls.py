8fcaaa2c4aa73275874208bd2c114a7e7e8b0814
Arnav <arnchkv@gmail.com>
2025-08-27 11:50:07 +0530

    todo urls moved to todo app from maim project

--- todo_site/todo/urls.py
+++ todo_site/todo/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-# from todo import views
+from django.urls import path
+from todo import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('todo/', include('todo.urls')),
-    # path('', views.index, name="todo"),
-    # path('del/<str:item_id>', views.remove, name="del"),
+    path('', views.index, name="todo"),
+    path('del/<str:item_id>', views.remove, name="del"),
 ]
